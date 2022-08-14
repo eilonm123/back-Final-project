e3c06731d0ee76494772ec9c899e8cd2e4c97ccc
eilon <eilonm123@gmail.com>
2022-08-14 16:34:10 +0300

    fix 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,18 +4,20 @@ const { sign } = require('jsonwebtoken');
 const secret = process.env.SECRET
 
 async function register(req, res) {
-	const user = await getUserByUsernameAndPassword(username, password);
-	if (user) {
-		throw new Error('user already exists')
-	}
-	else {
-		try {
-			const newUser = await createUser(req.body);
-			res.send(newUser);
-		} catch (err) {
-			res.status(400).send({ message: err.message });
-		}
-	}
+	const result = req.body
+	console.log(result)
+	// const user = await getUserByUsernameAndPassword(username, password);
+	// if (user) {
+	// 	throw new Error('user already exists')
+	// }
+	// else {
+	// 	try {
+	// 		const newUser = await createUser(req.body);
+	// 		res.send(newUser);
+	// 	} catch (err) {
+	// 		res.status(400).send({ message: err.message });
+	// 	}
+	// }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
-const {login} = require('../controllers/auth-controller')
+
+const { login, register } = require('../controllers/auth.controller');
 
 module.exports = function loadAuthRoutes(app) {
-	app.post('/api/register', register);
+	app.post('/api/login', login);
+	app.post('/api/register', register); 
+
 }
