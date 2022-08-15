28fee75ab7d9d26ceafb27f9b63abe39052d933c
eilon <eilonm123@gmail.com>
2022-08-15 15:43:52 +0300

    app work

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const {loadRoutes} = require('./routes');
+//loadRoutes(app);
+app.listen(3000,()=>console.log('app is running '));
 
-const loadRoutes = require('./routes');
-loadRoutes(app)
-
-app.listen(3000, () => {
-	console.log('listening on port 3000');
-})
